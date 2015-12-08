b8cf6c9ddb4f1c245c5936450e137cdd33e381c0
nmbryant <nmbryant@wpi.edu>
2015-12-08 14:26:18 -0500

    Adds an assets folder with some of the assets that will be used for the game. Creates the grid system for the game.

--- public/index.js
+++ public/index.js
@@ -5,6 +5,37 @@
 
 // Index.js takes care of game rules as well as event handlers.
 
+var gridWidth = 20;
+var gridHeight = 20;
+
+var FLOOR_VALUE = 0;
+var WALL_VALUE = 1;
+var POWER_PELLET_VALUE = 2;
+
+var mazeTable = [[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                 [1, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+                 [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                 [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                 [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                 [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                 [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                 [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                 [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                 [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                 [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                 [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                 [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                 [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                 [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                 [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                 [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                 [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                 [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                 [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]];
+
+createMaze();
+fillMaze();
+
 function displayTables()
 {
     var oReq = new XMLHttpRequest();
@@ -19,8 +50,49 @@ function requestListen()
     document.getElementById("thebody").innerHTML = this.responseText;
 }
 
-
 function addBodyListeners()
 {
 
+}
+
+function createMaze()
+{
+    var maze = document.getElementById("maze");
+    var html = "";
+    html += "<table class=\"tableCenter\">";
+    for (var i = 0; i < gridHeight; i++){
+        html += "<tr>";
+        for (var j = 0; j < gridWidth; j++){
+            var tableValue = mazeTable[i][j];
+            console.log(tableValue);
+            var squareClass = "";
+            if (tableValue == WALL_VALUE){
+                squareClass = "wall";
+            }
+            else {
+                squareClass = "floor";
+            }
+            html += "<td id=\"x" + i + "y" + j + "\"class=\"grid-square " + squareClass + "\">";
+            html += "</td>";
+        }
+        html += "</tr>";
+    }
+    html += "</table>";
+    maze.innerHTML = html;
+}
+
+function fillMaze()
+{
+    for (var i = 0; i < gridHeight; i++){
+        for (var j = 0; j < gridWidth; j++){
+            var tableValue = mazeTable[i][j];
+            var tableData = document.getElementById("x" + i + "y" + j);
+            if (tableValue == FLOOR_VALUE){
+                tableData.innerHTML = "<img src=\"assets/pellet.png\">";
+            }
+            else if (tableValue == POWER_PELLET_VALUE){
+                tableData.innerHTML = "<img src=\"assets/powerpellet.png\">";
+            }
+        }
+    }
 }
