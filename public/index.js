2f374722cb94773cbd0a0ae5fa622dea1860336a
jnallard <jnallard@wpi.edu>
2015-12-10 21:03:43 -0500

    Added socket.io to server

--- public/index.js
+++ public/index.js
@@ -6,6 +6,7 @@
 // Index.js takes care of game rules as well as event handlers.
 
 var isStartup = true;
+var socket = null;
 
 var gridWidth = 20;
 var gridHeight = 20;
@@ -172,9 +173,9 @@ function moveRight(){
     if (parseInt(imgObj.style.left) >= 600){
         clearTimeout(animate);
     }
-    console.log(imgObj.style.left);
+    //console.log(imgObj.style.left);
     var pacmanSquare = getSquareForObject("pacman-gif");
-    console.log("Pacman is at (" + pacmanSquare.x + ", " + pacmanSquare.y + ")");
+    //console.log("Pacman is at (" + pacmanSquare.x + ", " + pacmanSquare.y + ")");
 
     if (pacmanSquare.x != -1 && pacmanSquare.y != -1 && (pacmanSquare.x != pacmanStartingX || pacmanSquare.y != pacmanStartingY)) {
         var pacmanSquareData = mazeTable[pacmanSquare.x][pacmanSquare.y];
@@ -226,4 +227,82 @@ function sendNewTableData(tableX, tableY, newVal){
     var req = new XMLHttpRequest();
     req.open('POST', '/pacman');
     req.send('tableX=' + tableX + '&tableY=' + tableY + '&newVal=' + newVal);
+}
+
+connect();
+
+socket.emit('send update', "test");
+function connect(){
+    if(!socket){
+        socket = io();
+    }   
+    else{
+        socket.io.reconnect();
+    }
+
+    socket.on('new pacman update', function (data) {
+        pacman = parseObjectFromSockets(data.pacman);
+        console.log(pacman);
+        handlePacmanUpdate(pacman);
+    });
+    socket.on('new ghosts update', function (data) {
+        ghosts = parseObjectFromSockets(data.ghosts);
+        console.log(ghosts);
+        handleGhostsUpdate(ghosts);
+    });
+    socket.on('new board update', function (data) {
+        board = parseObjectFromSockets(data.board);
+        console.log(board);
+        handleBoardUpdate(board);
+    });
+}
+
+function sendObjectToSockets(updateName, object){
+    console.log(object);
+    socket.emit(updateName, JSON.stringify(object));
+}
+
+function parseObjectFromSockets(jsonString){
+    return JSON.parse(jsonString);
+}
+
+function sendPacmanUpdate(){
+    pacman = {oLocation: "oLocation here", direction: "direction here"};
+    sendObjectToSockets('send pacman update', pacman);
+}
+//sendPacmanUpdate();
+
+function handlePacmanUpdate(pacman){
+    oLocation = pacman.oLocation;
+    direction = pacman.direction;
+
+}
+
+function sendGhostsUpdate(){
+    ghost1 = {oLocation: "Ghost1 oLocation here", direction: "direction here"};
+    ghost2 = {oLocation: "oLocation here", direction: "direction here"};
+    ghost3 = {oLocation: "oLocation here", direction: "direction here"};
+    ghost4 = {oLocation: "oLocation here", direction: "direction here"};
+    ghosts = {ghost1: ghost1,ghost2: ghost2,ghost3: ghost3,ghost4: ghost4}
+    sendObjectToSockets('send ghosts update', ghosts);
+}
+//sendGhostsUpdate();
+
+function handleGhostsUpdate(ghosts){
+    ghost1 = ghosts.ghost1;
+    ghost2 = ghosts.ghost2;
+    ghost3 = ghosts.ghost3;
+    ghost4 = ghosts.ghost4;
+}
+
+sendBoardUpdate();
+function sendBoardUpdate(){
+    
+    board = {oLocation: "oLocation here", action: "action here"};
+    sendObjectToSockets('send board update', board);
+}
+
+function handleBoardUpdate(board){
+    oLocation = board.oLocation;
+    action = board.action;
 }
--- server.js
+++ server.js
@@ -2,11 +2,15 @@
 // CS 4241 Project 4
 
 var express = require('express');
-var http = require('http');
+//var http = require('http');
 var path = require('path');
 var fs = require('fs');
 var url = require('url');
 var csv = require('csv');
+var app = express();
+var port = process.env.PORT || 3000;
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
 
 var page = fs.readFileSync("./public/index.html").toString();
 var tablerows = "";
@@ -42,8 +46,6 @@ function initPage()
 
 }
 
-var app = express();
-var port = process.env.PORT || 3000;
 
 initPage();
 
@@ -72,9 +74,13 @@ app.post('/pacman', function(req, res){
     });
 });
 
-app.listen(port, function() {
+/*app.listen(port, function() {
     setInterval(testChangeValue, 1000);
     console.log('App is listening on port ' + port);
+});*/
+
+server.listen(port, function () {
+  console.log('Server listening at port %d', port);
 });
 
 app.use(express.static(path.join(__dirname, '/public')));
@@ -90,3 +96,31 @@ function testChangeValue(){
         mazeTable[i][j] = FLOOR_VALUE;
     }
 }
+
+var numUsers = 0;
+var users = [];
+
+io.on('connection', function (socket) {
+  var addedUser = false;
+
+  socket.on('send pacman update', function (data) {
+
+    socket.broadcast.emit('new pacman update', {
+      pacman: data
+    });
+  });
+
+  socket.on('send ghosts update', function (data) {
+
+    socket.broadcast.emit('new ghosts update', {
+      ghosts: data
+    });
+  });
+
+  socket.on('send board update', function (data) {
+
+    socket.broadcast.emit('new board update', {
+      board: data
+    });
+  });
+});
